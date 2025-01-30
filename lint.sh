875b4909bca6a16b0ee90ba28d6bfa46413dd529
Olivier Korach <olivier.korach@gmail.com>
2025-01-30 09:58:41 +0100

    Added colors

--- lint.sh
+++ lint.sh
@@ -2,6 +2,17 @@
 
 ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BUILDDIR="$ROOTDIR/build"
+YELLOW=$(tput setaf 3)
+GREEN=$(tput setaf 2)
+RESET=$(tput setaf 7)
+
+function log {
+    echo -e "${GREEN}$*${RESET}"
+}
+function logcmd {
+    echo -e "${YELLOW}$*${RESET}"
+}
+
 [ ! -d "$BUILDDIR" ] && mkdir "$BUILDDIR"
 rm -rf -- ${BUILDDIR:?"."}/* */__pycache__ */*.pyc
 
@@ -10,14 +21,18 @@ if [[ ! "$(which python)" =~ ^.*venv/bin/python$ ]]; then
     . venv/bin/activate
 fi
 
-echo "======= BUILDING PYTHON PACKAGE ========="
+log "\n======= BUILDING PYTHON PACKAGE ========="
+logcmd python -m build "\n"
 python -m build >/dev/null
 
-echo "======= BUILDING DOCKER IMAGE WITH PYTHON PACKAGE ========="
+log "\n======= BUILDING DOCKER IMAGE WITH PYTHON PACKAGE ========="
+logcmd docker build -t "olivierkorach/hello-world:1.0-snapshot" -t olivierkorach/hello-world:latest -f "$ROOTDIR/Dockerfile" "$ROOTDIR" --load "\n"
 docker build -t "olivierkorach/hello-world:1.0-snapshot" -t olivierkorach/hello-world:latest -f "$ROOTDIR/Dockerfile" "$ROOTDIR" --load
 
-echo "===> Running pylint"
+log "\n======= Running pylint ======="
 pylintReport="$BUILDDIR/pylint-report.out"
+logcmd pylint --rcfile "$ROOTDIR"/pylintrc "$ROOTDIR"/*.py "$ROOTDIR"/*/*.py -r n --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" \> "$pylintReport" "\n"
+
 pylint --rcfile "$ROOTDIR"/pylintrc "$ROOTDIR"/*.py "$ROOTDIR"/*/*.py -r n --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" > "$pylintReport"
 re=$?
 if [ "$re" == "32" ]; then
@@ -25,15 +40,23 @@ if [ "$re" == "32" ]; then
     exit $re
 fi
 
-echo "===> Running shellcheck and converting to SonarQube generic format on-the-fly"
+log "\n======= Running shellcheck and converting to SonarQube generic format on-the-fly ======"
+
 shellcheckReport="$BUILDDIR/generic-shellcheck.json"
+logcmd shellcheck "$ROOTDIR"/\*.sh -s bash -f json \| "$ROOTDIR"/shellcheck2sonar.py \> "$shellcheckReport" "\n"
 shellcheck "$ROOTDIR"/*.sh -s bash -f json | "$ROOTDIR"/shellcheck2sonar.py >"$shellcheckReport"
 
-echo "===> Running checkov"
+log "\n=======  Running checkov ======="
+
+logcmd checkov -d . --framework dockerfile -o sarif --output-file-path "$BUILDDIR" "\n"
 checkov -d . --framework dockerfile -o sarif --output-file-path "$BUILDDIR" >/dev/null
 
-echo "===> Running trivy"
+log "\n======= Running trivy ======="
+
 trivyReport="$BUILDDIR/generic-trivy.json"
+logcmd "trivy image -f json -o "$BUILDDIR"/trivy_results.json olivierkorach/hello-world:latest\n"
 trivy image -f json -o "$BUILDDIR"/trivy_results.json olivierkorach/hello-world:latest
-echo "===> Converting trivy report to SonarQube generic format"
+
+log "\n======= Converting trivy report to SonarQube generic format ======="
+logcmd python "$ROOTDIR"/trivy2sonar.py \< "$BUILDDIR"/trivy_results.json \> "$trivyReport" "\n"
 python "$ROOTDIR"/trivy2sonar.py < "$BUILDDIR"/trivy_results.json > "$trivyReport"
